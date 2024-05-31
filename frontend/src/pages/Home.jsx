02d51aff83f9511ca7d192fa3ba355ca5ea40455
JoJo777890 <JoJo777890@gmail.com>
2024-05-31 15:28:08 +0800

    2024-5-31 (Not Finished) Added Local Bootstrap
    Added Local Bootstrap, but not actually using it locally.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import api from "../api";
 import Note from "../components/Note"
 import "../styles/Home.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Home() {
     const [notes, setNotes] = useState([]);
