b685b39c9a5ace47b2d1e6e7cc43f5a7cc73ac31
JoJo777890 <JoJo777890@gmail.com>
2024-05-31 16:50:48 +0800

    2024-5-31 Added Table That Display User Info
    Added table that display user info.
    Not yet to change all names in the project that are about "note" to e.g. "UserInfo".
    
    Next Step:
    Attempt to create "UserInfoTable.jsx" component.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-// const apiUrl = " http://127.0.0.1:8000";
+const apiUrl = "http://127.0.0.1:8000";
 // const apiUrl = "http://django-startover4-env.eba-56rk2cit.us-west-2.elasticbeanstalk.com";
-const apiUrl = "https://api.bjpoker.net";
+// const apiUrl = "https://api.bjpoker.net";
 
 const api = axios.create({
   baseURL: apiUrl,
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -57,30 +57,59 @@ function Home() {
                     <Note note={note} onDelete={deleteNote} key={note.id} />
                 ))}
             </div>
-            <h2>Create a Note</h2>
-            <form onSubmit={createNote}>
-                <label htmlFor="title">Title:</label>
-                <br />
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    required
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                />
-                <label htmlFor="content">Content:</label>
-                <br />
-                <textarea
-                    id="content"
-                    name="content"
-                    required
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                ></textarea>
-                <br />
-                <input type="submit" value="Submit"></input>
-            </form>
+            {/*# (id: 1)*/}
+            {/*<h2>Create a Note</h2>*/}
+            {/*<form onSubmit={createNote}>*/}
+            {/*    <label htmlFor="title">Title:</label>*/}
+            {/*    <br />*/}
+            {/*    <input*/}
+            {/*        type="text"*/}
+            {/*        id="title"*/}
+            {/*        name="title"*/}
+            {/*        required*/}
+            {/*        onChange={(e) => setTitle(e.target.value)}*/}
+            {/*        value={title}*/}
+            {/*    />*/}
+            {/*    <label htmlFor="content">Content:</label>*/}
+            {/*    <br />*/}
+            {/*    <textarea*/}
+            {/*        id="content"*/}
+            {/*        name="content"*/}
+            {/*        required*/}
+            {/*        value={content}*/}
+            {/*        onChange={(e) => setContent(e.target.value)}*/}
+            {/*    ></textarea>*/}
+            {/*    <br />*/}
+            {/*    <input type="submit" value="Submit"></input>*/}
+            {/*</form>*/}
+
+            {/*# (id: 1)*/}
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                </thead>
+                <tbody>
+                {/*# (id: 2)*/}
+                {/*{notes.map((note) => (*/}
+                {/*    <Note note={note} onDelete={deleteNote} key={note.id} />*/}
+                {/*))}*/}
+
+                {/*# (id: 2)*/}
+                {notes.map((note) => (
+                    <tr key={note.id}>
+                        <th scope="row">{note.id}</th>
+                        <td>{note.title}</td>
+                        <td>{note.content}</td>
+                        <td>@mdo</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     );
 }
